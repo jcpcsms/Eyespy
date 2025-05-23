6b333940ca59a5f9d2d8d721c4a3ec44a95b5d01
Juan Paz <2024subject@gmail.com>
2025-05-23 19:07:50 -0400

    Update Action-Recognition-builder.py

--- Action-Recognition/Action-Recognition-builder.py
+++ Action-Recognition/Action-Recognition-builder.py
@@ -6,3 +6,12 @@ import os
 from datetime import datetime
 from collections import deque
 import mediapipe as mp 
+import torch
+import torch optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
+
+
+PYTORCH_AVAILABLE = True
+MEDIAPIPE_AVAILABLE = True
