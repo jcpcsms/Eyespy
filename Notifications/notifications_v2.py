83336428772d1529f2359abfbe5b68d936229da7
Juan Paz <carlitosflow@student.fullsail.edu>
2025-06-04 23:52:32 -0400

    Update notifications_v2.py

--- Notifications/notifications_v2.py
+++ Notifications/notifications_v2.py
@@ -9,10 +9,10 @@ def screenshot_payload(bot_token, chat_id, title, message):
   latest_file = max(files, key=os.path.getmtime)
   url = f"https://api.telegram.org/bot{bot_token}/sendPhoto"
   caption = f"{title}\n\n{message}"
-
+ 
   with open(latest_file, 'rb') as photo:
     response = requests.post(url, files={'photo': photo}, data={'chat_id': chat_id, 'caption': caption})
-
+  # verify response + error handling
   if response.status_code == 200:
     print("Sent to Telegram Server"
   else:
